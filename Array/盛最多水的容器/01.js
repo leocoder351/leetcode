b41dd8caa44f54d9bf89416080963152a3db7bed
liuxuan <liuxuan351@foxmail.com>
2020-03-03 23:33:04 +0800

    feat：添加盛最多水的解法1

--- Array/盛最多水的容器/01.js
+++ Array/盛最多水的容器/01.js
@@ -2,18 +2,18 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {
-  let max = 0;
-  let area = 0;
+var maxArea = function(height) {
+	let max = 0;
+	let area = 0;
 
-  for (let i = 1; i <= height.length; ++i) {
-    for (let j = i + 1; j <= height.length - 1; ++j) {
-      area = (j - i) * Math.min(height[i], height[j]);
-      if (max < area) max = area;
-    }
-  }
+	for (let i = 0; i < height.length; ++i) {
+		for (let j = i + 1; j < height.length; ++j) {
+			area = (j - i) * Math.min(height[i], height[j]);
+			if (max < area) max = area;
+		}
+	}
 
-  return max;
+	return max;
 };
 
 console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
